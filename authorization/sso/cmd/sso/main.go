1b355f3f3f08903c24d1dceb59b881ee969e88c9
Denis Beresnev <shanthi.bunch@gmail.com>
2024-08-05 23:11:36 +0300

    feat: added logger

--- authorization/sso/cmd/sso/main.go
+++ authorization/sso/cmd/sso/main.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/shanth1/golang-microservices/authorization/sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "development"
+	envProd  = "production"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+
+	log.Info("Ingo message", slog.String("string", "test"), slog.Int("int", 123))
+	log.Debug("Debug message")
+	log.Warn("Warn message")
+	log.Error("Error message")
+
 	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
