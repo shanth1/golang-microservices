659a79d71a875b8f975c6a97cd9d58a8bfa2b75a
Denis Beresnev <shanthi.bunch@gmail.com>
2024-08-24 18:02:07 +0300

    refactor: rename project to wg-fork-vpn

---
+++ wg-fork-vpn/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shanth1/golang-microservices/wg-fork-vpn/vpn"
+)
+
+func main() {
+	v, err := vpn.NewVpn()
+	if err != nil {
+		fmt.Printf("NewVpn error: %v\n", err)
+		return
+	}
+	defer v.Close()
+	v.Run()
+
+}
---
+++ wg-fork-vpn/tun/tun.go
@@ -0,0 +1,28 @@
+package tun
+
+type Event int
+
+const (
+	EventUp = 1 << iota
+	EventDown
+	EventMTUUpdate
+)
+
+type Tun struct {
+	T *NativeTun
+}
+
+func NewTun(name string, mtu int) (*Tun, error) {
+	t := new(Tun)
+
+	var err error
+	if t.T, err = CreateTUN(name, mtu); err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+func (t *Tun) Read() ([]byte, error) {
+	return t.T.ReadOne()
+}
