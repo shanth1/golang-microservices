c837db8a6dde2fe1a3e95808350464ca54857776
Denis Beresnev <shanthi.bunch@gmail.com>
2024-08-24 20:43:56 +0300

    feat: added validation on login handler

--- authorization/sso/internal/grpc/auth/server.go
+++ authorization/sso/internal/grpc/auth/server.go
@@ -5,6 +5,8 @@ import (
 
 	ssov1 "github.com/shanth1/golang-microservices/authorization/protos/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type serverAPI struct {
@@ -15,8 +17,28 @@ func Register(gRPC *grpc.Server) {
 	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
 }
 
-func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
-	return &ssov1.LoginResponse{Token: "Token " + req.GetEmail()}, nil
+const (
+	emptyValue = 0
+)
+
+func (s *serverAPI) Login(
+	ctx context.Context, req *ssov1.LoginRequest,
+) (*ssov1.LoginResponse, error) {
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "Email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "Password is required")
+	}
+
+	if req.AppId == emptyValue {
+		return nil, status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	return &ssov1.LoginResponse{
+		Token: "Token " + req.GetEmail(),
+	}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
