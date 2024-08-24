210e12df3f888ffc00524f9474377dd9a0590685
Denis Beresnev <shanthi.bunch@gmail.com>
2024-08-24 12:26:59 +0300

    feat: created Vpn struct and NewVpn func template

--- vpn/vpn/vpn.go
+++ vpn/vpn/vpn.go
@@ -1,5 +1,23 @@
 package vpn
 
-type Vpn struct{}
+import (
+	"github.com/shanth1/golang-microservices/vpn/conn"
+	"github.com/shanth1/golang-microservices/vpn/tun"
+)
+
+type Config struct {
+}
+
+type Vpn struct {
+	T   *tun.Tun
+	C   *conn.Conn
+	Cfg Config
+}
+
+func NewVpn() (*Vpn, error) {
+	v := new(Vpn)
+
+	return v, nil
+}
 
 func Main() {}
