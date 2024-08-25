648e208018e04a2a12559b81e8eb5396223d5697
Denis Beresnev <shanthi.bunch@gmail.com>
2024-08-25 20:16:05 +0300

    feat: sql migrations

---
+++ authorization/sso/cmd/migrator/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	// Library for migrations
+	"github.com/golang-migrate/migrate/v4"
+
+	// Driver for performing SQLite3 migrations
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+
+	// Driver for getting migrations from file
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	var storagePath, migrationsPath, migrationsTable string
+
+	flag.StringVar(&storagePath, "storage-path", "", "path to storage")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "path to migrations")
+	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "name of migration")
+	flag.Parse()
+
+	if storagePath == "" {
+		panic("storage-path is required")
+	}
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		fmt.Sprintf("sqlite3://%s?x-migrations-table=%s", storagePath, migrationsTable),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied successfully")
+}
