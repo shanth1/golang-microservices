2ef6791cfee647c6705c896414a49498a978e7ef
Denis Beresnev <shanthi.bunch@gmail.com>
2024-08-30 19:50:28 +0300

    feat: websocket connections

--- websocket-chat/main.go
+++ websocket-chat/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
 	"golang.org/x/net/websocket"
 )
 
@@ -14,5 +20,34 @@ func NewServer() *Server {
 	}
 }
 
+func (s *Server) HandleWS(ws *websocket.Conn) {
+	fmt.Printf("new inconming connection from client: %s", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.ReadLoop(ws)
+}
+
+func (s *Server) ReadLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		fmt.Println(string(msg))
+		ws.Write([]byte("thanks for the message!"))
+	}
+}
+
 func main() {
+	s := NewServer()
+	http.Handle("/ws", websocket.Handler(s.HandleWS))
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
