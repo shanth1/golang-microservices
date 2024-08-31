8bc522f4f6332af0e37030d9d879462735e63910
Denis Beresnev <shanthi.bunch@gmail.com>
2024-08-31 19:01:34 +0300

    feat: sending file logic

--- tcp-files/main.go
+++ tcp-files/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"time"
 )
 
 type fileServer struct {
@@ -38,7 +41,35 @@ func (fs *fileServer) readLoop(conn net.Conn) {
 	}
 }
 
+func sendFile(size int) error {
+	file := make([]byte, size)
+	if _, err := io.ReadFull(rand.Reader, file); err != nil {
+		return err
+	}
+
+	conn, err := net.Dial("tcp", ":3000")
+	if err != nil {
+		return err
+	}
+
+	n, err := conn.Write(file)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("written %d bytes\n", n)
+	return nil
+}
+
 func main() {
+	go func() {
+		time.Sleep(1 * time.Second)
+		if err := sendFile(1000); err != nil {
+			fmt.Println("error on sending file:", err)
+		}
+	}()
+
 	srv := &fileServer{}
 	srv.start()
+
 }
