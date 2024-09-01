350247ef18e6ea84da06e261e1f59fbaafb41235
Denis Beresnev <shanthi.bunch@gmail.com>
2024-09-01 17:37:34 +0300

    feat: seted up logger

--- url-shortener/cmd/url-shortener/main.go
+++ url-shortener/cmd/url-shortener/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/shanth1/golang-microservices/url-shortener/internal/config"
+)
+
+const (
+	envLocal   = "local"
+	envDevelop = "develop"
+	envProd    = "prod"
 )
 
 func main() {
@@ -12,10 +20,39 @@ func main() {
 	fmt.Println(cfg)
 
 	// TODO: init jlogger
+	logger := setupLogger(cfg.Env)
+	logger.Info("starting url shortener", slog.String("env", cfg.Env))
+	logger.Debug("debug messages are enabled")
 
 	// TODO: init storage
 
 	// TODO: init router
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(
+			os.Stdout,
+			&slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}))
+	case envDevelop:
+		logger = slog.New(slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{
+				Level: slog.LevelInfo,
+			}))
+	}
+
+	return logger
 }
